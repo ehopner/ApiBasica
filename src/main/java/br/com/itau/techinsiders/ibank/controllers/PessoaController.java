56d91b7363e0ba47e448af92f39a80be0ee4c4be
EDUARDO HOPNER PEREIRA <987270434@itaud.des.ihf>
2020-06-10 11:37:11 -0300

    adicionado endpoint consulta telefone

--- src/main/java/br/com/itau/techinsiders/ibank/controllers/PessoaController.java
+++ src/main/java/br/com/itau/techinsiders/ibank/controllers/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.itau.techinsiders.ibank.models.Pessoa;
@@ -43,6 +44,13 @@ public class PessoaController {
     public Pessoa buscaPessoas(@PathVariable("id") Long idPessoa) {
         Optional<Pessoa> optionalPessoa = pessoaRepository.findById(idPessoa);
         return optionalPessoa.get();
+
+    }
+
+    @GetMapping(path = "/pessoas/telefone")
+    public Optional<Pessoa> buscarPorTelefone(@RequestParam("numero") String telefone) {
+        Optional<Pessoa> pessoa = pessoaRepository.findPessoaByTelefone(telefone);
+        return pessoa;
 
     }
 
--- src/main/java/br/com/itau/techinsiders/ibank/repository/PessoaRepository.java
+++ src/main/java/br/com/itau/techinsiders/ibank/repository/PessoaRepository.java
@@ -1,9 +1,13 @@
 package br.com.itau.techinsiders.ibank.repository;
- 
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
  
 import br.com.itau.techinsiders.ibank.models.Pessoa;
  
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+
+    public Optional<Pessoa> findPessoaByTelefone (String telefone);
  
 }

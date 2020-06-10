9a14bcad37fee5c4e3ff8f884ab3384983c06433
Eduardo Hopner <ehpemc@gmail.com>
2020-06-09 23:36:40 -0300

    ajustado cors headers

--- src/main/java/br/com/itau/techinsiders/ibank/controllers/PessoaController.java
+++ src/main/java/br/com/itau/techinsiders/ibank/controllers/PessoaController.java
@@ -1,6 +1,7 @@
 package br.com.itau.techinsiders.ibank.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +16,14 @@ public class PessoaController {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500/")
     @PostMapping(path = "/pessoas", consumes = "application/json", produces = "application/json")
     public Pessoa addPessoa(@RequestBody final Pessoa novaPessoa) {
         //PessoaRepository pr = new PessoaRepository()
         final Pessoa pessoaInserida = pessoaRepository.save(novaPessoa);
         return pessoaInserida;
-        
+        //http://127.0.0.1:5500/
+        //https://spring.io/guides/gs/rest-service-cors/
     }
 
     @GetMapping(path = "/pessoas", produces = "application/json")

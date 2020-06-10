50a74a96641f20d57ac3b4b759b9debf73cfeb41
Eduardo Hopner <ehpemc@gmail.com>
2020-06-09 21:51:30 -0300

    tudo funfando

--- src/main/java/br/com/itau/techinsiders/ibank/models/Pessoa.java
+++ src/main/java/br/com/itau/techinsiders/ibank/models/Pessoa.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.GenericGenerator;
+// import org.hibernate.annotations.GenericGenerator;
  
 @Entity
 public class Pessoa implements Serializable {
